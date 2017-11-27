8206462bc9b5bce7bee5e45a6a670f20bd2dd991
Roy Macdonald <macdonald.roy@gmail.com>
2017-11-27 02:22:53 -0500

    ofxSoundObject: added override to some methods, formatting and minor changes

--- src/ofxSoundObject.cpp
+++ src/ofxSoundObject.cpp
@@ -5,16 +5,15 @@
  */
 
 #include "ofxSoundObject.h"
-
-//----------------------------------------------------
-// ofxSoundObject
-
+#include "ofxSoundObjectsUtils.h"
+//--------------------------------------------------------------
+//  ofxSoundObject
+//--------------------------------------------------------------
 ofxSoundObject::ofxSoundObject() {
 	inputObject = NULL;
     outputObjectRef = NULL;
 }
-
-
+//--------------------------------------------------------------
 ofxSoundObject &ofxSoundObject::connectTo(ofxSoundObject &soundObject) {
     if (outputObjectRef != NULL) {
         disconnect();
@@ -29,26 +28,26 @@ ofxSoundObject &ofxSoundObject::connectTo(ofxSoundObject &soundObject) {
 	}
 	return soundObject;
 }
-
+//--------------------------------------------------------------
 void ofxSoundObject::disconnectInput(ofxSoundObject * input){
     if (inputObject != NULL) {
     inputObject = NULL;
     }
 }
-
+//--------------------------------------------------------------
 void ofxSoundObject::disconnect(){
     outputObjectRef->disconnectInput(this);
     outputObjectRef =NULL;
 }
-
+//--------------------------------------------------------------
 void ofxSoundObject::setInput(ofxSoundObject *obj) {
 	inputObject = obj;
 }
-
+//--------------------------------------------------------------
 ofxSoundObject *ofxSoundObject::getInputObject() {
 	return inputObject;
 }
-
+//--------------------------------------------------------------
 bool ofxSoundObject::checkForInfiniteLoops() {
 	ofxSoundObject *prev = inputObject;
 
@@ -60,48 +59,44 @@ bool ofxSoundObject::checkForInfiniteLoops() {
 	// if we found ourselves, return false (to indicate there's an infinite loop)
 	return (prev==NULL);
 }
-
+//--------------------------------------------------------------
 // this pulls the audio through from earlier links in the chain
 void ofxSoundObject::audioOut(ofSoundBuffer &output) {
+    ofxSoundObjects::checkBuffers(output, workingBuffer);
 	if(inputObject!=NULL) {
-		if(workingBuffer.size()!=output.size()) {
-            ofLogVerbose("ofxSoundObject") << "working buffer size != output buffer size. " << workingBuffer.size() << " != " <<output.size();
-            workingBuffer.resize(output.size());
-            workingBuffer.setNumChannels(output.getNumChannels());
-            workingBuffer.setSampleRate(output.getSampleRate());
-		}
-
 		inputObject->audioOut(workingBuffer);
 	}
 	this->process(workingBuffer, output);
 }
-
-//----------------------------------------------------
-// ofSoundInput
-
-ofxSoundInput::ofxSoundInput() {
-
+////--------------------------------------------------------------
+//void ofxSoundObject::setNumChannels(int num){
+//    numChannels = num;
+//}
+////--------------------------------------------------------------
+//int ofxSoundObject::getNumChannels() const{
+//    return numChannels;
+//}
+//--------------------------------------------------------------
+ofSoundBuffer& ofxSoundObject::getBuffer(){
+return workingBuffer;
 }
-
+//--------------------------------------------------------------
+const ofSoundBuffer& ofxSoundObject::getBuffer() const{
+return workingBuffer;    
+}
+//--------------------------------------------------------------
+//  ofxSoundInput
+//--------------------------------------------------------------
+ofxSoundInput::ofxSoundInput() {
+}
+//--------------------------------------------------------------
 // copy audio in to internal buffer
 void ofxSoundInput::audioIn(ofSoundBuffer &input) {
-	if(inputBuffer.size()!=input.size()) {
-        ofLogVerbose("ofSoundinput::audioIn") << "input buffer size != output buffer size.";
-		inputBuffer.resize(input.size());
-        inputBuffer.setNumChannels(input.getNumChannels());
-        inputBuffer.setSampleRate(input.getSampleRate());
-	}
+    ofxSoundObjects::checkBuffers(input, inputBuffer);
 	input.copyTo(inputBuffer);
 }
-
+//--------------------------------------------------------------
 void ofxSoundInput::audioOut(ofSoundBuffer &output) {
-    if(output.getNumFrames()==inputBuffer.getNumFrames()){
-        ofLogVerbose("ofSoundinput::audioOut") << "input buffer size != output buffer size.";
-        inputBuffer.resize(output.size());
-        inputBuffer.setNumChannels(output.getNumChannels());
-        inputBuffer.setSampleRate(output.getSampleRate());
-	}
+    ofxSoundObjects::checkBuffers(output, inputBuffer);
 	inputBuffer.copyTo(output);
 }
-
-
--- src/ofxSoundObject.h
+++ src/ofxSoundObject.h
@@ -8,6 +8,9 @@
 // (while allowing backwards compatibility with previous versions of openFrameworks)
 #define OF_SOUND_OBJECT_AVAILABLE
 
+//--------------------------------------------------------------
+//  ofxSoundObject
+//--------------------------------------------------------------
 /**
  * ofxSoundObject is a node in your dsp chain. It can have one input,
  * and one output. If it doesn't have an input, it's the beginning
@@ -34,7 +37,7 @@ public:
 	/// this pulls the audio through from earlier links in the chain.
 	/// you can override this to add more interesting functionality
 	/// like signal splitters, sidechains etc.
-	virtual void audioOut(ofSoundBuffer &output);
+	virtual void audioOut(ofSoundBuffer &output) override;
 
 	/// this checks the dsp chain to ensure there are no infinite loops
 	/// - might want to override this if you make a splitter
@@ -42,12 +45,24 @@ public:
 	virtual bool checkForInfiniteLoops();
 	ofxSoundObject *getInputObject();
 
+    /// This sets/gets the number of channels that this sound object should process.
+    /// By default it will use the number of channels from the ofSoundBuffer passed by the previous link in the chain.
+    /// For a 2 channel setup there should be no need to set this, it is mainly for scenarios with more inputs or outputs.
+//    virtual void setNumChannels(int num);
+//    virtual int getNumChannels() const;
+
+    ofSoundBuffer& getBuffer();
+    const ofSoundBuffer& getBuffer() const;
+
 protected:
 	// this is the previous dsp object in the chain
 	// that feeds this one with input.
 	ofxSoundObject *inputObject;
     ofxSoundObject *outputObjectRef;
     virtual void setInput(ofxSoundObject *obj);
+    
+    
+//    int numChannels = 0;
 
 private:
 	// ofxSoundObjects reference their source, not their destination
@@ -58,6 +73,9 @@ private:
 	ofSoundBuffer workingBuffer;
 };
 
+//--------------------------------------------------------------
+//  ofxSoundInput
+//--------------------------------------------------------------
 /**
  * This class represents input from the sound card in your dsp chain.
  */
@@ -65,10 +83,10 @@ class ofxSoundInput: public ofBaseSoundInput, public ofxSoundObject {
 public:
 	ofxSoundInput();
 	// copy audio in to internal buffer
-	void audioIn(ofSoundBuffer &input);
-	void audioOut(ofSoundBuffer &output);
-
-private:
+	virtual void audioIn(ofSoundBuffer &input) override;
+	virtual void audioOut(ofSoundBuffer &output) override;
+        
+protected:
 	ofSoundBuffer inputBuffer;
 };
 
