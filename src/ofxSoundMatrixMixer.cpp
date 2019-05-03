51c985491d67210ab057ecf3f812c808424f48df
Roy Macdonald <macdonald.roy@gmail.com>
2019-05-03 15:42:16 -0400

    matrix mixer initial implementation. unfinished yet.

--- src/ofxSoundMatrixMixer.cpp
+++ src/ofxSoundMatrixMixer.cpp
@@ -6,4 +6,142 @@
 //
 //
 
-#include "ofxSoundMatrixMixer.hpp"
+#include "ofxSoundMatrixMixer.h"
+
+//----------------------------------------------------
+ofxSoundMatrixMixer::ofxSoundMatrixMixer():ofxSoundObject(OFX_SOUND_OBJECT_PROCESSOR){
+	masterVolume = 1.0f;	
+	masterVol.set("Master Vol", 1, 0, 1);
+	masterVol.addListener(this, &ofxSoundMatrixMixer::masterVolChanged);
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::masterVolChanged(float& f) {
+	mutex.lock();
+	masterVolume = masterVol;
+	mutex.unlock();
+}
+//----------------------------------------------------
+ofxSoundMatrixMixer::~ofxSoundMatrixMixer(){
+	masterVol.removeListener(this, &ofxSoundMatrixMixer::masterVolChanged);
+}
+//----------------------------------------------------
+ofxSoundObject* ofxSoundMatrixMixer::getInputObject(size_t objectNumber){
+	if (objectNumber < inObjects.size()) {
+		return inObjects[objectNumber].obj;
+	}else{
+		return nullptr;
+	}
+}
+//----------------------------------------------------
+size_t ofxSoundMatrixMixer::getNumInputChannels(){
+}
+//----------------------------------------------------
+size_t ofxSoundMatrixMixer::getNumInputObjects(){
+}
+//----------------------------------------------------
+ofxSoundObject* ofxSoundMatrixMixer::getInputChannelSource(size_t channelNumber){
+	if (channelNumber < inObjects.size()) {
+		return inObjects[channelNumber].obj;
+	}else{
+		return nullptr;
+	}
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::disconnectInput(ofxSoundObject * input){
+	ofRemove(inObjects, [&](MatrixInputObject& o){return input == o.obj;});
+//	for (int i =0; i<input.size(); i++) {
+//		if (input == channels[i]) {
+//			channels.erase(channels.begin() + i);
+////			channelVolume.erase(channelVolume.begin() + i);
+//			break;
+//		}
+//	}
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::setInput(ofxSoundObject *obj){
+	if(obj){
+	for (int i =0; i<inObjects.size(); i++) {
+		if (obj == inObjects[i].obj) {
+			ofLogNotice("ofxSoundMatrixMixer::setInput") << " already connected" ;
+			return;
+		}
+	}
+	inObjects.push_back(MatrixInputObject(obj, numOutputChannels));
+	updateNumInputChannels();
+//		for(auto & i : inObjects){
+			
+//			i.resize(numInputChannels);
+//			inObjectsVolumes.push_back(std::vector<float>( numInputChannels, 0.0f));
+//		}
+	}
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::updateNumOutputChannels(const size_t & nc){
+	if(numOutputChannels != nc){
+		numOutputChannels = nc;
+		for(auto& i : inObjects){
+			i.updateChanVolsSize(numOutputChannels);
+		}
+	}
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::updateNumInputChannels(){
+
+	numInputChannels = 0;
+	
+	for(auto& i : inObjects){
+		numInputChannels += i.obj->getBuffer().getNumChannels();
+	}
+	
+	
+}
+//----------------------------------------------------
+void ofxSoundMatrixMixer::setMasterVolume(float vol){
+	mutex.lock();
+	masterVolume = vol;
+	mutex.unlock();
+}
+//----------------------------------------------------
+float ofxSoundMatrixMixer::getMasterVolume(){
+	return masterVolume;
+}
+//----------------------------------------------------
+bool ofxSoundMatrixMixer::isConnected(ofxSoundObject& obj){
+	for (int i =0; i<inObjects.size(); i++) {
+		if (&obj == inObjects[i].obj) {
+			return true;
+		}
+	}
+	return false;
+}
+//----------------------------------------------------
+// this pulls the audio through from earlier links in the chain and sums up the total output
+void ofxSoundMatrixMixer::audioOut(ofSoundBuffer &output) {
+	updateNumOutputChannels(output.getNumChannels());
+	if(inObjects.size()>0) {
+		for(int i = 0; i < inObjects.size(); i++){
+			if (inObjects[i].obj != NULL ) {
+				ofSoundBuffer tempBuffer;
+				tempBuffer.resize(output.size());
+//				tempBuffer.setNumChannels(inObjects[i].obj);
+				tempBuffer.setSampleRate(output.getSampleRate());
+				inObjects[i].obj->audioOut(tempBuffer);//could this be done on different threads? maybe threadChannels?
+				
+				for(auto& c : inObjects[i].channelsVolumes){
+					
+				}
+				
+//				float v = channelVolume[i];
+				for (int j = 0; j < tempBuffer.size(); j++) {
+					output.getBuffer()[j] += tempBuffer.getBuffer()[j] * v;
+				}
+			}
+		}
+		/*   if(output.getNumChannels() == 2) {
+		 output.stereoPan(1-masterPan, masterPan);
+		 }*/
+		output*=masterVolume;
+	}
+//	numOutputChannels = output.getNumChannels();
+}
+//----------------------------------------------------
--- src/ofxSoundMatrixMixer.h
+++ src/ofxSoundMatrixMixer.h
@@ -1,14 +1,81 @@
 //
-//  ofxSoundMatrixMixer.hpp
+//  ofxSoundMatrixMixer.h
 //  example-matrixMixer
 //
 //  Created by Roy Macdonald on 4/28/19.
 //
 //
 
-#ifndef ofxSoundMatrixMixer_hpp
-#define ofxSoundMatrixMixer_hpp
+#pragma once
+#include <vector>
+#include "ofParameter.h"
+#include "ofxSoundObject.h"
+#include "ofTypes.h"
 
-#include <stdio.h>
+class ofxSoundMatrixMixer: public ofxSoundObject{
+public:
+	
+	ofxSoundMatrixMixer();
+	virtual ~ofxSoundMatrixMixer();
+	
+	ofxSoundObject* getInputChannelSource(size_t channelNumber);
+	ofxSoundObject* getInputObject(size_t objectNumber);
+	size_t getNumInputChannels();
+	size_t getNumInputObjects();
+	/// sets output volume multiplier.
+	/// a volume of 1 means "full volume", 0 is muted.
+	void setMasterVolume(float vol);
+	float getMasterVolume();
+	
+	
+	void audioOut(ofSoundBuffer &output) override;
+	bool isConnected(ofxSoundObject& obj);
+	
+//	void setChannelVolume(int channelNumber, float vol);
+//	float getChannelVolume(int channelNumber);
+//	
 
-#endif /* ofxSoundMatrixMixer_hpp */
+	ofParameter<float> masterVol;
+protected:
+	struct MatrixInputObject{//this is just an auxiliary struct to keep things tidier 
+		void updateChanVolsSize(const size_t& numOutChanns){
+			if(obj){
+				if(channelsVolumes.size() != obj->getBuffer().getNumChannels()){
+					channelsVolumes.resize(obj->getBuffer().getNumChannels(), std::vector<float>(numOutChanns, 0.0f));
+				}
+				for(auto& c: channelsVolumes){
+					if(c.size() != numOutChanns) 
+						c.resize(numOutChanns);
+				}
+			}
+		}
+		MatrixInputObject(ofxSoundObject* _obj, const size_t& numOutChanns):obj(_obj){
+			updateChanVolsSize(numOutChanns);
+		}
+		
+		ofxSoundObject* obj;
+		
+		std::vector< std::vector<float> > channelsVolumes; //[in channel index] [ output channel index] 
+	};
+	
+//	void setupMixers();
+//	std::vector<ofxSoundMixer> mixers;
+	void masterVolChanged(float& f);
+	void disconnectInput(ofxSoundObject * input) override;
+	std::vector<MatrixInputObject>inObjects;
+//	vector<ofxSoundObject*>inObjects;
+//	std::vector< std::vector<float> >inObjectsVolumes; //[ output channel index ] [ input channel index ] 
+	size_t numInputChannels = 0;
+	size_t numOutputChannels = 0;
+	void updateNumInputChannels();
+	void updateNumOutputChannels(const size_t & nc);
+//	vector<float> channelVolume;
+//	float masterPan;
+	float masterVolume;
+	void setInput(ofxSoundObject *obj) override;
+	ofMutex mutex;
+
+	
+	
+	
+};
