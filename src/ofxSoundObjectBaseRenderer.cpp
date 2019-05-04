556a6476eef00b1c4afee57e68b5da82071751cf
Roy Macdonald <macdonald.roy@gmail.com>
2019-05-04 16:58:40 -0400

    added base renderer object

---
+++ src/ofxSoundObjectBaseRenderer.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxSoundObjectBaseRenderer.cpp
+//  example-matrixMixer
+//
+//  Created by Roy Macdonald on 5/3/19.
+//
+//
+
+#include "ofxSoundObjectBaseRenderer.hpp"
---
+++ src/ofxSoundObjectBaseRenderer.h
@@ -0,0 +1,14 @@
+//
+//  ofxSoundObjectBaseRenderer.hpp
+//  example-matrixMixer
+//
+//  Created by Roy Macdonald on 5/3/19.
+//
+//
+
+#ifndef ofxSoundObjectBaseRenderer_hpp
+#define ofxSoundObjectBaseRenderer_hpp
+
+#include <stdio.h>
+
+#endif /* ofxSoundObjectBaseRenderer_hpp */
