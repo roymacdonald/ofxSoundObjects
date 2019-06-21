635decaad5a0e2657af7f3d6d127f919ea8e7019
Roy Macdonald <macdonald.roy@gmail.com>
2019-06-20 22:08:22 -0400

    example-soundPlayerObject-> added a lot of comments in code and made the mesh renderirng clearer

--- example-soundPlayerObject/src/ofApp.cpp
+++ example-soundPlayerObject/src/ofApp.cpp
@@ -4,11 +4,25 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 	ofSetLogLevel(OF_LOG_VERBOSE);
+	ofBackground(0);
 	
-//	ofFileDialogResult result = ofSystemLoadDialog();
-//	if (result.bSuccess) {
-//        player.load(result.getPath());
-	player.load("/Users/roy/Downloads/jun12/2019-06-12-18-02-41-602_chan_15.wav");
+	ofFileDialogResult result = ofSystemLoadDialog();
+	if (result.bSuccess) {
+		// the second argument passed to player.load is a boolean indicating if you want to 
+		// open the file in streaming mode (default is false). This means that the audio data
+		// is not actually read and loaded from disk into memory when you call player.load, 
+		// instead it is being read from the file on disk as your app requests it. It really 
+		// depends on the scenario which mode is better. Non streaming mode will load the 
+		// complete file into ram memory, thus its memory footprint can be large if the file 
+		// size is large (it can actually be much larger than the file size itself). So if you
+		// load a lot of files which are large it might be better to use the streaming mode. 
+		// On the other hand, streaming mode will allocate a very small amount of memory, 
+		// although it will read from the disk directly on demand and perform any decompresion
+		// and sound format conversion, which might be less performant than non streaming mode.
+		// The difference in terms of performance between these two modes depends on your 
+		// computer so you might want to bench mark both. 
+        player.load(result.getPath(), true);
+	
 		
 		
 		//----- Sound stream setup begin -------.
@@ -19,15 +33,10 @@ void ofApp::setup(){
 		auto outDevices = ofxSoundUtils::getOutputSoundDevices();
 		
 		// IMPORTANT!!!
-		// The following two lines of code is where you set which audio interface to use.
+		// In the following line of code is where you set which audio interface to use.
 		// the index is the number printed in the console inside [ ] before the interface name 
-		// You can use a different input and output device.
-		
 		
 		int outDeviceIndex = 0;
-		
-		cout << ofxSoundUtils::getSoundDeviceString(outDevices[outDeviceIndex], false, true) << endl;
-		
 		
 		ofSoundStreamSettings soundSettings;
 		soundSettings.numInputChannels = 0;
@@ -35,26 +44,37 @@ void ofApp::setup(){
 		soundSettings.sampleRate = player.getSoundFile().getSampleRate();
 		soundSettings.bufferSize = 256;
 		soundSettings.numBuffers = 1;
-		
+		soundSettings.setOutDevice(outDevices[outDeviceIndex]);
+	
         stream.setup(soundSettings);
 		
 		
 		// it is important to set up which object is going to deliver the audio data to the sound stream.
-		// thus, we need to set the stream's output. The output object is going to be the last one of the audio signal chain, which is set up further down
+		// thus, we need to set the stream's output. The output object is going to be the last one of 
+		// the audio signal chain, which is set up further down.
 		stream.setOutput(output);
-//		stream.setOutput(player);
 		//-------Sound stream setup end -------.
 		
-		// the waveformDraw receives the rectangle where it is going to be drawn
+		// waveformDraw::setup receives the rectangle where it is going to be drawn
+		// fullFileWaveform will be in the top 1/3 of the screen.
+  
 		fullFileWaveform.setup(0,0, ofGetWidth(), ofGetHeight()/3);
-		
+		// wave will be on the lower 2/3 of the screen
+		// wave will render the waveform of the sound data as it is passing through the signal change.
 		wave.setup(0, fullFileWaveform.getMaxY(), ofGetWidth(), ofGetHeight() - fullFileWaveform.getMaxY());
-        
+		
+		// The fullFileWaveform will render a static mesh, which does not change over time. 
+		// waveformDraw has a handy function which will create its internal meshes from an ofSoundBuffer
+		// object passed. In this case we pass to it the complete sound buffer of the sound file loaded.
+		
+		fullFileWaveform.makeMeshFromBuffer( player.getSoundFile().getBuffer());
+		
 		
 		// Audio signal chain setup.
 		// Each of our objects need to connect to each other in order to create a signal chain, which ends with the output; the object that we set as the sound stream output.
-//		player.connectTo(wave).connectTo(output);
-		player.connectTo(output);
+		// notice that here we dont connect fulFileWaveform. It does not need to.
+		player.connectTo(wave).connectTo(output);
+
         
 		
 
@@ -62,43 +82,12 @@ void ofApp::setup(){
         gui.setup();
         gui.add(pan.set("PAN", 0, -1,1));
         gui.add(player.volume);
-        pan.addListener(this, &ofApp::panChanged);
         gui.add(speed.set("Speed", 1, 0, 10));
 		
 		//----- gui listeners
+		pan.addListener(this, &ofApp::panChanged);
 		speed.addListener(this, &ofApp::speedChanged);
 		// ------- GUI setup end -------
-		
-		//
-		fullFileWaveform.makeMeshFromBuffer( player.getSoundFile().getBuffer());
-		
-		ofBackground(0);
-		
-		// Also we are going to read the data from the sound player and create a mesh to draw its waveform
-		// we will create one waveform for each channel in the sound player
-
-//		waveforms.resize(player.getSoundFile().getNumChannels());
-//		float h = ofGetHeight()/waveforms.size();
-		
-		
-		// we need to get the sound buffer from the sound file in order to get the whole file's data.
-		// calling player.getBuffer(), which actually is a function, will return the players current buffer, the one that is being sent to the sound device, so it will not work for what we are trying to achieve.
-//		ofSoundBuffer& buff = player.getSoundFile().getBuffer();
-		
-//		for(int i = 0; i < waveforms.size(); i++){
-//			ofSoundBuffer b;
-//			buff.getChannel(b, i); // we need to extract each channel from the sound buffer
-//			
-//			waveforms[i].setMode(OF_PRIMITIVE_LINE_STRIP);
-//			for(int j = 0; j < b.getBuffer().size(); j++ ){
-//				// map the buffer sample x position so we can fit the whole file into the app's window
-//				float x = ofMap(j, 0, b.getBuffer().size(), 0, ofGetWidth());
-//				// The sound data comes as a normalized float, which means that its values can go from -1 to 1.
-//				// We want to map these values into the desired height of our mesh, and position it acordingly.
-//				float y = ofMap(b.getBuffer()[j], -1, 1, i*h, (i+1)*h);
-//				waveforms[i].addVertex({x, y,0.0});
-//			}
-//		}
 		
 		player.play();
 			 
@@ -110,7 +99,7 @@ void ofApp::setup(){
 			// if the player is not looping you can register  to the end event, which will get triggered when the player reaches the end of the file. 
 			playerEndListener = player.endEvent.newListener(this, &ofApp::playerEnded);
 		}
-//	}
+	}
 	
 }
 //--------------------------------------------------------------
@@ -126,7 +115,6 @@ void ofApp::speedChanged(float&){
 }
 //--------------------------------------------------------------
 void ofApp::panChanged(float&f){
-
     player.setPan(pan);
 }
 //--------------------------------------------------------------
@@ -142,9 +130,7 @@ void ofApp::draw(){
 	ofSetColor(ofColor::white);
 	
 	fullFileWaveform.draw();
-//	for(auto&w:waveforms){
-//		w.draw();
-//	}
+
 	ofSetColor(ofColor::red);
 	float playhead = ofMap(player.getPosition(), 0,1,0,ofGetWidth()); 
 	ofDrawLine(playhead, 0, playhead, ofGetHeight());
@@ -159,6 +145,8 @@ void ofApp::draw(){
 
 
     gui.draw();
+	
+	ofDrawBitmapStringHighlight("click any where on the window to set the playhead", 20, ofGetHeight() - 30); 
 	
 }
 //--------------------------------------------------------------
