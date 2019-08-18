68e33539a7f4ebc4d1cc1831a4db42b2d832b925
Roy Macdonald <macdonald.roy@gmail.com>
2019-08-18 19:17:50 -0400

    added slider grid class used in matrix renderer

---
+++ src/Renderers/ofxSlidersGrid.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxSlidersGrid.cpp
+//  example-matrixMixer_tbb
+//
+//  Created by Roy Macdonald on 8/17/19.
+//
+//
+
+#include "ofxSlidersGrid.hpp"
---
+++ src/Renderers/ofxSlidersGrid.h
@@ -0,0 +1,14 @@
+//
+//  ofxSlidersGrid.hpp
+//  example-matrixMixer_tbb
+//
+//  Created by Roy Macdonald on 8/17/19.
+//
+//
+
+#ifndef ofxSlidersGrid_hpp
+#define ofxSlidersGrid_hpp
+
+#include <stdio.h>
+
+#endif /* ofxSlidersGrid_hpp */
