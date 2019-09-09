ec26987b1b69e6c5e9faf9780b82bab3b465cbec
Roy Macdonald <macdonald.roy@gmail.com>
2019-09-08 23:57:02 -0400

    waveformDraw: added ofRectangle param to draw method so it can be defined during the draw method. It will only update the meshes if these need to. makeMeshFromBuffer now receives a const buffer and builds the meshes on the draw function

--- src/SoundObjects/waveformDraw.h
+++ src/SoundObjects/waveformDraw.h
@@ -27,30 +27,13 @@ public:
 		bRenderWaveforms = true;
         input.copyTo(output);
 	}
+	
 //--------------------------------------------------------------
-    void draw(){
-//        if (buffer.size() >0) {
-//            vector<ofMesh>meshes;
-//			int chans = buffer.getNumChannels();
-//			for (int i = 0; i < chans; i++) {
-//				meshes.push_back(ofMesh());
-//				meshes.back().setMode(OF_PRIMITIVE_LINE_STRIP);
-//			}
-//            
-//			float h = this->height / float(chans);
-//			 
-//            float xInc = this->getWidth()/(float)(buffer.getNumFrames() -1);
-//            ofVec3f v;
-//            v.x = x;
-//            for(int i=0; i<buffer.getNumFrames(); i++){
-//                for (int j = 0; j < chans; j++) {
-//                    v.y = ofMap(buffer[i*chans + j], -1, 1, h*(j+1) + y, h*j + y );
-//                    meshes[j].addVertex(v);
-//                }
-//                v.x += xInc;
-//			}
-//			
-		
+    void draw(const ofRectangle& viewport = ofRectangle()){
+		if(!viewport.isZero() && viewport != (ofRectangle)*this){
+			this->set(viewport);
+			bRenderWaveforms = true;
+		}
 		if(bRenderWaveforms){
 			bRenderWaveforms = false;
 			makeWaveformMesh();
@@ -61,15 +44,15 @@ public:
     }
 //--------------------------------------------------------------
 	// you can use this method to render and draw a static buffer.
-	void makeMeshFromBuffer(ofSoundBuffer& buffer){
+	void makeMeshFromBuffer(const ofSoundBuffer& buffer){
 		{
 			std::lock_guard<std::mutex> lck(mutex1);
 			this->buffer.resize(buffer.size());
 			this->buffer.setNumChannels(buffer.getNumChannels());
 			this->buffer.setSampleRate(buffer.getSampleRate());
 			buffer.copyTo(this->buffer);
+			bRenderWaveforms = true;
 		}
-		makeWaveformMesh();
 	}
 protected:
 	void makeWaveformMesh(){
