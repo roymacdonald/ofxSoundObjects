f374e674f4243c86f709b1fea317f54168e055ad
Roy Macdonald <macdonald.roy@gmail.com>
2021-05-06 11:12:35 -0400

    added ofxSoundPlayerObject.h with a typedef to ofxMultiSoundPlayer to avoid breaking older projects

---
+++ src/ofxSoundPlayerObject.h
@@ -0,0 +1,10 @@
+//
+//  ofxSoundPlayerObject.h
+//
+//  Created by Roy Macdonald on 5/6/21.
+//
+
+#pragma once
+#include "ofxMultiSoundPlayer.h"
+
+typedef ofxMultiSoundPlayer ofxSoundPlayerObject;
