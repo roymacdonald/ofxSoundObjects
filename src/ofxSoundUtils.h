818bf9c00cf964c712a95a207a5d1251cbdb9cae
Roy Macdonald <macdonald.roy@gmail.com>
2022-08-21 16:31:52 -0500

    ofCircularBuffer. Properly allocating buffer when pushing into it

--- src/ofxSoundUtils.h
+++ src/ofxSoundUtils.h
@@ -40,17 +40,38 @@ class ofxCircularSoundBuffer: public ofSoundBuffer{
 public:
 	
 	void push(ofSoundBuffer& buffer){
+        if(size() == 0 || getNumChannels() == 0 || getNumFrames() != buffer.getNumFrames() * numBuffers){
+            allocate(buffer.getNumFrames() * numBuffers, buffer.getNumChannels());
+            setSampleRate(buffer.getSampleRate());
+        }
 		if(getBuffer().size() > 0){
-			pushIndex %= getBuffer().size();
-			
-			memcpy(&getBuffer()[pushIndex], &buffer.getBuffer()[0], sizeof(float) * buffer.getBuffer().size());
-			
-			pushIndex += buffer.getBuffer().size();
+            pushIndex += lastPushSize;
+            
+            lastPushSize = buffer.getBuffer().size();
+            if(pushIndex + lastPushSize <= getBuffer().size()){
+                memcpy(&getBuffer()[pushIndex], &buffer.getBuffer()[0], sizeof(float) * lastPushSize);
+            }else{
+                size_t n = getBuffer().size() - pushIndex;
+                memcpy(&getBuffer()[pushIndex], &buffer.getBuffer()[0], sizeof(float) * n);
+                memcpy(&getBuffer()[0], &buffer.getBuffer()[0], sizeof(float) * (lastPushSize - n));
+            }
+            pushIndex %= getBuffer().size();
+
+
 		}
 	}
-	size_t getPushIndex() const {return pushIndex; }
+	// returns the index at which the last push was done. This means, the index of the newest data.
+    size_t getPushIndex() const {return pushIndex; }
+    
+    
+    /// get/set the number of buffers stored by the circular buffer. this is the amount of times push can be called before starting to overrite the oldest data
+    void setNumBuffersToStore(size_t n){numBuffers = n;}
+    size_t getNumBuffersToStore(){return numBuffers;}
 	
 private:
-	
+    size_t numBuffers = 100;
+    
+    size_t lastPushSize = 0;
+    
 	size_t pushIndex = 0;
 };
